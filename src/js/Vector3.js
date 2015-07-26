3d846728a0c7c22d61494f47a46b8e671668814b
Yuki Shimada <yukiema@gmail.com>
2015-07-26 21:17:47 +0900

    Vector3クラスにlength, lengthSquared, cross, normalizeメソッドを実装した。

--- src/js/Vector3.js
+++ src/js/Vector3.js
@@ -8,6 +8,78 @@ export default class Vector3 {
     this.z = z;
   }
 
+  /**
+   * 長さ
+   */
+  length() {
+    return Math.sqrt(this.x*this.x + this.y*this.y + this.z*this.z);
+  }
+
+  /**
+   * 長さ（static版）
+   */
+  static length(vec3) {
+    return Math.sqrt(vec3.x*vec3.x + vec3.y*vec3.y + vec3.z*vec3.z);
+  }
+
+  /**
+   * 長さの2乗
+   */
+  lengthSquared() {
+    return this.x*this.x + this.y*this.y + this.z*this.z;
+  }
+
+  /**
+   * 長さの2乗（static版）
+   */
+  lengthSquared(vec3) {
+    return vec3.x*vec3.x + vec3.y*vec3.y + vec3.z*vec3.z;
+  }
+
+  /**
+   * 外積
+   */
+  cross(v) {
+    var x = this.y*v.z - this.z*v.y;
+    var y = this.z*v.x - this.x*v.z;
+    var z = this.x*v.y - this.y*v.x;
+
+    this.setComponents(x, y, z);
+
+    return this;
+  }
+
+  /**
+  * 外積(static版)
+  */
+  static cross(lv, rv) {
+    var x = lv.y*rv.z - lv.z*rv.y;
+    var y = lv.z*rv.x - lv.x*rv.z;
+    var z = lv.x*rv.y - lv.y*rv.x;
+
+    return new Vector3(x, y, z);
+  }
+
+  /**
+   * 正規化
+   */
+  normalize() {
+    var length = this.length();
+    this.div(length);
+
+    return this;
+  }
+
+  /**
+   * 正規化（static版）
+   */
+  static normalize(vec3) {
+    var length = vec3.length();
+    vec3.div(length);
+
+    return vec3;
+  }
+
 }
 
 GLBoost["Vector3"] = Vector3;
