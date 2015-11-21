08ade5175a31321b50f56e90c3c134b1f38c5c8a
Yuki Shimada <yukiema@gmail.com>
2015-11-21 18:33:51 +0900

    add the Matrix3x3.js.

---
+++ src/js/Matrix3x3.js
@@ -0,0 +1,255 @@
+import GLBoost from './globals'
+import Vector3 from './Vector3'
+
+export default class Matrix3x3 {
+
+  constructor() {
+    this.m = [];
+    if (arguments.length >= 9) {
+      this.setComponents.apply(this, arguments);
+    }
+    else {
+      this.identity();
+    }
+  }
+
+  setComponents(m00, m01, m02, m10, m11, m12, m20, m21, m22) {
+    this.m00 = m00; this.m01 = m01; this.m02 = m02;
+    this.m10 = m10; this.m11 = m11; this.m12 = m12;
+    this.m20 = m20; this.m21 = m21; this.m22 = m22;
+
+    return this;
+  }
+
+  /**
+   * 単位行列にする
+   */
+  identity() {
+    this.setComponents(
+      1, 0, 0,
+      0, 1, 0,
+      0, 0, 1
+    );
+    return this;
+  }
+
+  /**
+   * 単位行列にする（static版）
+   */
+  static identity() {
+    return new Matrix3x3(
+      1, 0, 0,
+      0, 1, 0,
+      0, 0, 1
+    );
+  }
+
+
+  /**
+   * Create Y oriented Rotation Matrix
+   */
+  rotateY(radian) {
+    var cos = Math.cos(radian);
+    var sin = Math.sin(radian);
+    this.setComponents(
+      cos, 0, sin,
+      0, 1, 0,
+      -sin, 0, cos
+    );
+    return this;
+  }
+  /**
+   * Create Y oriented Rotation Matrix
+   */
+  static rotateY(radian) {
+    var cos = Math.cos(radian);
+    var sin = Math.sin(radian);
+    return new Matrix3x3(
+      cos, 0, sin,
+      0, 1, 0,
+      -sin, 0, cos
+    );
+  }
+
+  /**
+   * ゼロ行列
+   */
+  zero() {
+    this.setComponents(0, 0, 0, 0, 0, 0, 0, 0, 0);
+    return this;
+  }
+
+  static zero() {
+    return new Matrix3x3(0, 0, 0, 0, 0, 0, 0, 0, 0);
+  }
+
+  flatten() {
+    return this.m;
+  }
+
+  _swap(l, r) {
+    this.m[r] = [this.m[l], this.m[l] = this.m[r]][0]; // Swap
+  }
+
+  /**
+   * 転置
+   */
+  transpose() {
+    this._swap(1, 3);
+    this._swap(2, 6);
+    this._swap(5, 8);
+
+    return this;
+  }
+
+  /**
+   * 転置（static版）
+   */
+  static transpose(mat) {
+
+    var mat_t = new Matrix3x3(
+      mat.m00, mat.m10, mat.m20,
+      mat.m01, mat.m11, mat.m21,
+      mat.m02, mat.m12, mat.m22
+    );
+
+    return mat_t;
+  }
+
+  multiplyVector(vec) {
+    var x = this.m00*vec.x + this.m01*vec.y + this.m02*vec.z;
+    var y = this.m10*vec.x + this.m11*vec.y + this.m12*vec.z;
+    var z = this.m20*vec.x + this.m21*vec.y + this.m22*vec.z;
+
+    return new Vector4(x, y, z);
+  }
+
+  /**
+   * 行列同士の乗算
+   */
+  multiply(mat) {
+    var m00 = this.m00*mat.m00 + this.m01*mat.m10 + this.m02*mat.m20;
+    var m01 = this.m00*mat.m01 + this.m01*mat.m11 + this.m02*mat.m21;
+    var m02 = this.m00*mat.m02 + this.m01*mat.m12 + this.m02*mat.m22;
+
+    var m10 = this.m10*mat.m00 + this.m11*mat.m10 + this.m12*mat.m20;
+    var m11 = this.m10*mat.m01 + this.m11*mat.m11 + this.m12*mat.m21;
+    var m12 = this.m10*mat.m02 + this.m11*mat.m12 + this.m12*mat.m22;
+
+    var m20 = this.m20*mat.m00 + this.m21*mat.m10 + this.m22*mat.m20;
+    var m21 = this.m20*mat.m01 + this.m21*mat.m11 + this.m22*mat.m21;
+    var m22 = this.m20*mat.m02 + this.m21*mat.m12 + this.m22*mat.m22;
+
+
+    return this.setComponents(
+      m00, m01, m02,
+      m10, m11, m12,
+      m20, m21, m22
+    );
+  }
+
+  /**
+   * 行列同士の乗算（static版）
+   */
+  static multiply(l_m, r_m) {
+    var m00 = l_m.m00*r_m.m00 + l_m.m01*r_m.m10 + l_m.m02*r_m.m20;
+    var m10 = l_m.m10*r_m.m00 + l_m.m11*r_m.m10 + l_m.m12*r_m.m20;
+    var m20 = l_m.m20*r_m.m00 + l_m.m21*r_m.m10 + l_m.m22*r_m.m20;
+
+    var m01 = l_m.m00*r_m.m01 + l_m.m01*r_m.m11 + l_m.m02*r_m.m21;
+    var m11 = l_m.m10*r_m.m01 + l_m.m11*r_m.m11 + l_m.m12*r_m.m21;
+    var m21 = l_m.m20*r_m.m01 + l_m.m21*r_m.m11 + l_m.m22*r_m.m21;
+
+    var m02 = l_m.m00*r_m.m02 + l_m.m01*r_m.m12 + l_m.m02*r_m.m22;
+    var m12 = l_m.m10*r_m.m02 + l_m.m11*r_m.m12 + l_m.m12*r_m.m22;
+    var m22 = l_m.m20*r_m.m02 + l_m.m21*r_m.m12 + l_m.m22*r_m.m22;
+
+    return new Matrix3x3(
+      m00, m01, m02,
+      m10, m11, m12,
+      m20, m21, m22
+    );
+  }
+
+  /*
+   toMatrix3x3() {
+   return new Matrix3x3.js
+   }
+   */
+
+  set m00(val) {
+    this.m[0] = val;
+  }
+
+  get m00() {
+    return this.m[0];
+  }
+
+  set m01(val) {
+    this.m[1] = val;
+  }
+
+  get m01() {
+    return this.m[1];
+  }
+
+  set m02(val) {
+    this.m[2] = val;
+  }
+
+  get m02() {
+    return this.m[2];
+  }
+
+
+  set m10(val) {
+    this.m[3] = val;
+  }
+
+  get m10() {
+    return this.m[3];
+  }
+
+  set m11(val) {
+    this.m[4] = val;
+  }
+
+  get m11() {
+    return this.m[4];
+  }
+
+  set m12(val) {
+    this.m[5] = val;
+  }
+
+  get m12() {
+    return this.m[5];
+  }
+
+  set m20(val) {
+    this.m[6  ] = val;
+  }
+
+  get m20() {
+    return this.m[6];
+  }
+
+  set m21(val) {
+    this.m[7] = val;
+  }
+
+  get m21() {
+    return this.m[7];
+  }
+
+  set m22(val) {
+    this.m[8] = val;
+  }
+
+  get m22() {
+    return this.m[8];
+  }
+
+}
+
+GLBoost["Matrix3x3"] = Matrix3x3;
