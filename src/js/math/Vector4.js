02ad02eabe94882406ce9bd2df224145e5bde873
Yuki Shimada <yukiema@gmail.com>
2015-12-09 02:14:35 +0900

    add isEqual method to Vector4.

--- src/js/math/Vector4.js
+++ src/js/math/Vector4.js
@@ -9,6 +9,13 @@ export default class Vector4 {
     this.w = w;
   }
 
+  isEqual(vec) {
+    if (this.x === vec.x && this.y === vec.y && this.z === vec.z && this.w === vec.w) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 GLBoost["Vector4"] = Vector4;
