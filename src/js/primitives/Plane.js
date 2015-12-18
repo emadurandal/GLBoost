cb51d903c82c5f977784247f9c3e69905c954498
Yuki Shimada <yukiema@gmail.com>
2015-12-18 10:36:55 +0900

    (refs #21) Extends plane mesh class to specify subdivisions

--- src/js/primitives/Plane.js
+++ src/js/primitives/Plane.js
@@ -8,44 +8,69 @@ import Vector3 from './../math/Vector3'
 import Vector2 from './../math/Vector2'
 
 export default class Plane extends Geometry {
-  constructor(width, height, vertexColor, canvas) {
+  constructor(width, height, uSpan, vSpan, vertexColor, canvas) {
     super(canvas);
 
     Plane._instanceCount = (typeof Plane._instanceCount === "undefined") ? 0 : (Plane._instanceCount + 1);
 
-    this._setupVertexData(width/2.0, height/2.0, vertexColor);
+    this._setupVertexData(width, height, uSpan, vSpan, vertexColor);
   }
 
-  _setupVertexData(halfWidth, halfHeight, vertexColor) {
-    var indices = [
-      3, 1, 0, 2, 1, 3
-    ];
+  _setupVertexData(width, height, uSpan, vSpan, vertexColor) {
 
-    var positions = [
-      new Vector3(-halfWidth, 0, -halfHeight),
-      new Vector3(halfWidth,  0, -halfHeight),
-      new Vector3(halfWidth,  0, halfHeight),
-      new Vector3(-halfWidth, 0, halfHeight)
-    ];
-    var colors = [
-      new Vector4(vertexColor.x, vertexColor.y, vertexColor.z, vertexColor.w),
-      new Vector4(vertexColor.x, vertexColor.y, vertexColor.z, vertexColor.w),
-      new Vector4(vertexColor.x, vertexColor.y, vertexColor.z, vertexColor.w),
-      new Vector4(vertexColor.x, vertexColor.y, vertexColor.z, vertexColor.w)
-    ];
-    var texcoords = [
-      new Vector2(0.0, 0.0),
-      new Vector2(1.0, 0.0),
-      new Vector2(1.0, 1.0),
-      new Vector2(0.0, 1.0)
-    ];
+    var positions = [];
+
+    for(let i=0; i<=vSpan; i++) {
+      for(let j=0; j<=uSpan; j++) {
+        positions.push(new Vector3((j/uSpan - 1/2)*width, 0, (i/vSpan - 1/2)*height));
+      }
+    }
+
+    var indices = [];
+    for(let i=0; i<vSpan; i++) {
+      let degenerate_left_index = 0;
+      let degenerate_right_index = 0;
+      for(let j=0; j<=uSpan; j++) {
+        indices.push(i*(uSpan+1)+j);
+        indices.push((i+1)*(uSpan+1)+j);
+        if (j === 0) {
+          degenerate_left_index = (i + 1) * (uSpan+1) + j;
+        } else if (j === uSpan) {
+          degenerate_right_index = (i + 1) * (uSpan+1) + j;
+        }
+      }
+      indices.push(degenerate_right_index);
+      indices.push(degenerate_left_index);
+    }
+    var colors = [];
+    for(let i=0; i<=vSpan; i++) {
+      for(let j=0; j<=uSpan; j++) {
+        colors.push(vertexColor);
+      }
+    }
+
+    var texcoords = [];
+    for(let i=0; i<=vSpan; i++) {
+      for(let j=0; j<=uSpan; j++) {
+        texcoords.push(new Vector2(j/uSpan, i/vSpan));
+      }
+    }
+
+    var normal = new Vector3(0, 1, 0);
+    var normals = [];
+    for(let i=0; i<=vSpan; i++) {
+      for(let j=0; j<=uSpan; j++) {
+        normals.push(normal);
+      }
+    }
 
     this.setVerticesData({
       position: positions,
       color: colors,
       texcoord: texcoords,
+      normal: normals,
       indices: [indices]
-    });
+    }, GLBoost.TRIANGLE_STRIP);
   }
 
   toString() {
