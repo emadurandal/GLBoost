7859a88490d09d82218b9e97b697881a82ffd1f1
Yuki Shimada <yukiema@gmail.com>
2016-01-30 21:46:16 +0900

    Add Switching WebGL versions (1.0 / 2.0) in all examples.

--- examples/standalone/particles/app.js
+++ examples/standalone/particles/app.js
@@ -80,6 +80,15 @@
   };
 
   babelHelpers;
+  var arg = new Object();
+  var pair = location.search.substring(1).split('&');
+  for (var i = 0; pair[i]; i++) {
+    var kv = pair[i].split('=');
+    arg[kv[0]] = kv[1];
+  }
+
+  GLBoost.TARGET_WEBGL_VERSION = arg.webglver ? parseInt(arg.webglver) : 1;
+
   var canvas = document.getElementById("world");
 
   var renderer = new GLBoost.Renderer({ canvas: canvas, clearColor: { red: 0.5, green: 0.5, blue: 0.5, alpha: 1 } });
@@ -112,18 +121,18 @@
     }
 
     babelHelpers.createClass(MyCustomShaderSource, [{
-      key: "VSDefine_MyCustomShaderSource",
+      key: 'VSDefine_MyCustomShaderSource',
       value: function VSDefine_MyCustomShaderSource(in_, out_, f, lights) {
         var shaderText = '';
 
-        shaderText += in_ + " vec3 aVertex_" + attributeName + ";\n";
-        shaderText += "uniform float time;\n";
-        shaderText += "uniform float endHeight;\n";
+        shaderText += in_ + ' vec3 aVertex_' + attributeName + ';\n';
+        shaderText += 'uniform float time;\n';
+        shaderText += 'uniform float endHeight;\n';
 
         return shaderText;
       }
     }, {
-      key: "VSTransform_MyCustomShaderSource",
+      key: 'VSTransform_MyCustomShaderSource',
       value: function VSTransform_MyCustomShaderSource(existCamera_f, f, lights) {
         var shaderText = '';
         shaderText += '  vec3 gravity = vec3(0.0, -9.8, 0.0);\n';
@@ -136,7 +145,7 @@
         return shaderText;
       }
     }, {
-      key: "FSShade_MyCustomShaderSource",
+      key: 'FSShade_MyCustomShaderSource',
       value: function FSShade_MyCustomShaderSource(f, gl, lights) {
         var shaderText = '';
 
@@ -145,7 +154,7 @@
         return shaderText;
       }
     }, {
-      key: "prepare_MyCustomShaderSource",
+      key: 'prepare_MyCustomShaderSource',
       value: function prepare_MyCustomShaderSource(gl, shaderProgram, vertexAttribs, existCamera_f, lights) {
 
         var vertexAttribsAsResult = [];
@@ -181,9 +190,9 @@
     }
 
     babelHelpers.createClass(MyCustomShader, [{
-      key: "setUniforms",
+      key: 'setUniforms',
       value: function setUniforms(gl, glslProgram, material) {
-        babelHelpers.get(Object.getPrototypeOf(MyCustomShader.prototype), "setUniforms", this).call(this, gl, glslProgram, material);
+        babelHelpers.get(Object.getPrototypeOf(MyCustomShader.prototype), 'setUniforms', this).call(this, gl, glslProgram, material);
 
         gl.uniform1f(glslProgram.time, this._time);
         gl.uniform1f(glslProgram.endHeight, -3.5);
@@ -191,7 +200,7 @@
         return true;
       }
     }, {
-      key: "increaseTime",
+      key: 'increaseTime',
       value: function increaseTime(delta) {
         this._time += delta;
       }
@@ -202,7 +211,7 @@
   var particlesPosition = [];
   var particlesVelocity = [];
 
-  for (var i = 0; i < 100; i++) {
+  for (var _i = 0; _i < 100; _i++) {
     particlesPosition.push(new GLBoost.Vector3(Math.random(), Math.random(), Math.random()));
     particlesVelocity.push(new GLBoost.Vector3((Math.random() - 0.5) * 4, Math.random() * 10, (Math.random() - 0.5) * 4));
   }
