272da965e4dab578c3c55d4450a6f422d0f31fb0
Yuki Shimada <yukiema@gmail.com>
2016-02-27 09:02:54 +0900

    Add esdoc style comments to Scene class

--- src/js/Scene.js
+++ src/js/Scene.js
@@ -5,6 +5,12 @@ import AbstractLight from './lights/AbstractLight'
 import Mesh from './Mesh'
 import Group from './Group'
 
+/**
+ * en: This Scene class is the top level element of scene graph hierarchy.
+ *       To render scene, pass this scene element to Renderer.draw method.<br>
+ * ja: このSceneクラスはシーングラフ階層のトップレベルに位置する要素です。
+ *       シーンをレンダリングするには、このscene要素をRenderer.drawメソッドに渡します。
+ */
 export default class Scene extends Element {
   constructor() {
     super();
@@ -22,16 +28,31 @@ export default class Scene extends Element {
     }
   }
 
+  /**
+   * en: Add the element to this scene as a child.<br>
+   * ja: このシーンにelementを子供として追加します。
+   * @param element a instance of Element class
+   */
   add(element) {
     this._elements.push(element);
     element._parent = this;
   }
 
+  /**
+   * en: Add the element to this scene as a child.<br>
+   * ja: このシーンにelementを子供として追加します。
+   * @param element a instance of Element class
+   */
   addChild(element) {
     this._elements.push(element);
     element._parent = this;
   }
 
+  /**
+   * en: Get child elements which belong to this scene.<br>
+   * ja: このシーンに属していた子供の要素の配列を返します。
+   * @return {Array} en: child elements of this scene. ja: このシーンの子供の要素
+   */
   getChildren() {
     return this._elements;
   }
@@ -54,12 +75,21 @@ export default class Scene extends Element {
     return this._currentAnimationInputValues[inputName];
   }
 
+  /**
+   * en: Set animation input value (for instance frame value). This value affect all child elements in this scene graph (recursively).<br>
+   * ja: アニメーションのための入力値（例えばフレーム値）をセットします。この値はシーングラフに属する全ての子孫に影響します。
+   * @param {string} inputName en: inputName name of input value. ja: 入力値の名前
+   * @param {any} inputValue en: input value of animation. ja: アニメーションの入力値
+   */
   setCurrentAnimationValue(inputName, inputValue) {
     this._setDirtyToAnimatedElement(inputName);
     this._currentAnimationInputValues[inputName] = inputValue;
   }
 
-
+  /**
+   * en: Prepare for Rendering. You have to call this method before Renderer.draw method.
+   * ja: レンダリングのための前処理を行います。Renderer.drawメソッドの前にこのメソッドを呼ぶ必要があります。
+   */
   prepareForRender() {
     // カメラが最低１つでも存在しているか確認
     var existCamera_f = false;
@@ -142,18 +172,38 @@ export default class Scene extends Element {
 
   }
 
+  /**
+   * en: Get child elements which belong to this scene.<br>
+   * ja: このシーンに属していた子供の要素の配列を返します。
+   * @return {Array} en: child elements of this scene. ja: このシーンの子供の要素
+   */
   get elements() {
     return this._elements;
   }
 
+  /**
+   * en: Get child meshes which belong to this scene.<br>
+   * ja: このシーンに属していた子供のMesh要素の配列を返します。
+   * @return {Array} en: child meshes of this scene. ja: このシーンの子供のMesh要素
+   */
   get meshes() {
     return this._meshes;
   }
 
+  /**
+   * en: Get child lights which belong to this scene.<br>
+   * ja: このシーンに属していた子供のLight要素の配列を返します。
+   * @return {Array} en: child lights of this scene. ja: このシーンの子供のLight要素
+   */
   get lights() {
     return this._lights;
   }
 
+  /**
+   * en: Get child cameras which belong to this scene.<br>
+   * ja: このシーンに属していた子供のCamera要素の配列を返します。
+   * @return {Array} en: child cameras of this scene. ja: このシーンの子供のCamera要素
+   */
   get cameras() {
     return this._cameras;
   }
