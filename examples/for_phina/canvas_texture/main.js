479a4a0025fd21611dc9f176771f42d75ab828d1
Yuki Shimada <yukiema@gmail.com>
2016-11-20 14:59:01 +0900

    Update canvas_texture example

--- examples/for_phina/canvas_texture/main.js
+++ examples/for_phina/canvas_texture/main.js
@@ -62,22 +62,24 @@ phina.define('MainScene', {
     });
 
     var texture = glBoostContext.createPhinaTexture(512, 512, new GLBoost.Vector4(1, 0, 1, 0.2));
-    
-    var label = Label('phina.jsとGLBoostの夢の共演！');
+    this.texture = texture;
+    var label = Label('');
     label.fill = 'white';
     label.stroke = 'black';
     label.fontSize = 28;
     label.strokeWidth = 4;
+    label.align = 'left';
     label.x = label.calcCanvasWidth()/2;
     label.y = label.calcCanvasHeight()/2;
     label.y += 100;
-    
+    this.label = label;
+
     var heart = HeartShape({radius: 86});
     heart.x = heart.calcCanvasWidth()/2;
     heart.y = heart.calcCanvasHeight()/2;
     heart.x += 130;
     heart.y += 150;
-    
+
     texture.addPhinaObject(label).addPhinaObject(heart).renderPhinaObjects();
 
     var material = glBoostContext.createClassicMaterial();
@@ -86,32 +88,50 @@ phina.define('MainScene', {
     layer.scene.addChild( mesh );
 
     var camera = glBoostContext.createPerspectiveCamera(
-        {
-          eye: new GLBoost.Vector3(0.0, 0, 2.0),
-          center: new GLBoost.Vector3(0.0, 0.0, 0.0),
-          up: new GLBoost.Vector3(0.0, 1.0, 0.0)
-        },
-        {
-          fovy: 45.0,
-          aspect: 1.0,
-          zNear: 0.1,
-          zFar: 300.0
-        }
+      {
+        eye: new GLBoost.Vector3(0.0, 0, 2.0),
+        center: new GLBoost.Vector3(0.0, 0.0, 0.0),
+        up: new GLBoost.Vector3(0.0, 1.0, 0.0)
+      },
+      {
+        fovy: 45.0,
+        aspect: 1.0,
+        zNear: 0.1,
+        zFar: 300.0
+      }
     );
     layer.scene.addChild( camera );
 
     layer.expression.prepareToRender();
 
     var delta = 0;
-    layer.update = function() {
+
+    layer.time = 0;
+    layer.textCount = 0;
+    layer.displayText = 'phina.jsとGLBoostの夢の共演！';
+
+    layer.update = function(app) {
 
       mesh.rotate = new GLBoost.Vector3(0, delta, 0);
 
-      delta += 0.3;
-        /*
-      var rotateMatrix = GLBoost.Matrix44.rotateY(-1.0);
-      var rotatedVector = rotateMatrix.multiplyVector(camera.eye);
-      camera.eye = rotatedVector;*/
+      //
+      this.time += app.deltaTime;
+
+      if (this.time > 100) {
+        this.textCount++;
+        label.text = this.displayText.substr(0, this.textCount);
+        texture.renderPhinaObjects();
+        this.time = 0;
+        if (this.textCount > this.displayText.length + 10) {
+          this.textCount = 0;
+        }
+      }
+
+      delta += 0.1;
+      /*
+       var rotateMatrix = GLBoost.Matrix44.rotateY(-1.0);
+       var rotatedVector = rotateMatrix.multiplyVector(camera.eye);
+       camera.eye = rotatedVector;*/
     };
 
 
