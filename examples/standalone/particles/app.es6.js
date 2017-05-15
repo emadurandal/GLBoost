426c2f30a137e662f3b13eb7b55c864b1cb250ee
Yuki Shimada <emadurandal@gmail.com>
2017-05-16 00:19:40 +0900

    Fix an examples

--- examples/standalone/particles/app.es6.js
+++ examples/standalone/particles/app.es6.js
@@ -107,8 +107,8 @@ class MyCustomShader extends GLBoost.HalfLambertShader {
     this._time = 0;
   }
 
-  setUniforms(gl, glslProgram, expression, material) {
-    super.setUniforms(gl, glslProgram, expression, material);
+  setUniforms(gl, glslProgram, expression, material, camera, mesh, lights) {
+    super.setUniforms(gl, glslProgram, expression, material, camera, mesh, lights);
 
     gl.uniform1f(glslProgram.time, this._time);
     gl.uniform1f(glslProgram.endHeight, -3.5);
