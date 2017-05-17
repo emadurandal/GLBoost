728afa6d88efc77c2a5b1b9c0bfef25578102076
Yuki Shimada <emadurandal@gmail.com>
2017-05-17 17:55:27 +0900

    [SPV] Fix an error when not using any depth texture

--- src/js/middle_level/shaders/SPVPhongShader.js
+++ src/js/middle_level/shaders/SPVPhongShader.js
@@ -29,10 +29,11 @@ export class SPVPhongShaderSource {
     shaderText += '  vec3 normal = normalize(v_normal);\n';
 
     for (let i=0; i<lights.length; i++) {
+      let isShadowEnabledAsTexture = (lights[i].camera && lights[i].camera.texture) ? true:false;
       shaderText += `  {\n`;
       // if PointLight: lightPosition[i].w === 1.0      if DirectionalLight: lightPosition[i].w === 0.0
       shaderText += `    vec3 light = normalize(lightPosition[${i}].xyz - position.xyz * lightPosition[${i}].w);\n`;
-      shaderText +=      Shader._generateShadowingStr(gl, i);
+      shaderText +=      Shader._generateShadowingStr(gl, i, isShadowEnabledAsTexture);
       shaderText += `    float diffuse = max(dot(light, normal), 0.0);\n`;
       shaderText += `    rt0 += vec4(visibility, visibility, visibility, 1.0) * Kd * lightDiffuse[${i}] * vec4(diffuse, diffuse, diffuse, 1.0) * surfaceColor;\n`;
       shaderText += `    vec3 view = normalize(viewPosition - position.xyz);\n`;
