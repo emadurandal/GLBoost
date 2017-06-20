f387ca7e27802b4cf35362b884639c4a15d1aa8a
Yuki Shimada <emadurandal@gmail.com>
2017-06-21 01:04:16 +0900

    Add support line count of Arrow primitive

--- src/js/low_level/primitives/Arrow.js
+++ src/js/low_level/primitives/Arrow.js
@@ -4,36 +4,39 @@ import Vector3 from '../../low_level/math/Vector3';
 import Vector4 from '../../low_level/math/Vector4';
 
 export default class Arrow extends Geometry {
-  constructor(glBoostContext, length) {
+  constructor(glBoostContext, length, lineCount = 1) {
     super(glBoostContext);
 
-    this._setupVertexData(length);
+    this._setupVertexData(length, lineCount);
   }
 
-  _setupVertexData(length) {
+  _setupVertexData(length, lineCount) {
 
     let arrowheadWidth = length/10;
     let arrowheadLength = length/7.5;
     let stickLength = length - arrowheadLength;
     let halfLength = length/2;
 
-    let positions = [
+    let positions = [];
+
+    for (let i=0; i<lineCount; i++) {
+      let lineOffset = i - lineCount/2;
       // Stick part
-      new Vector3(0, 0, 0+halfLength),
-      new Vector3(0, 0, -stickLength+halfLength),
+      positions.push(new Vector3(0, lineOffset, halfLength));
+      positions.push(new Vector3(0, lineOffset, -stickLength+halfLength));
 
       // 1st line of a triangle
-      new Vector3(arrowheadWidth, 0, -stickLength+halfLength),
-      new Vector3(-arrowheadWidth, 0, -stickLength+halfLength),
+      positions.push(new Vector3(arrowheadWidth, lineOffset, -stickLength+halfLength));
+      positions.push(new Vector3(-arrowheadWidth, lineOffset, -stickLength+halfLength));
 
       // 2nd line of a triangle
-      new Vector3(-arrowheadWidth, 0, -stickLength+halfLength),
-      new Vector3(0, 0, -length+halfLength),
+      positions.push(new Vector3(-arrowheadWidth, lineOffset, -stickLength+halfLength));
+      positions.push(new Vector3(0, lineOffset, -length+halfLength));
 
       // 3rd line of a triangle
-      new Vector3(0, 0, -length+halfLength),
-      new Vector3(arrowheadWidth, 0, -stickLength+halfLength),
-    ];
+      positions.push(new Vector3(0, lineOffset, -length+halfLength));
+      positions.push(new Vector3(arrowheadWidth, lineOffset, -stickLength+halfLength));
+    }
 
     this.setVerticesData({
       position: positions
