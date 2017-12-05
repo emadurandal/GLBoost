13fa01599392e14334c5299f0a3f9efd51b5474b
Yuki Shimada <emadurandal@gmail.com>
2017-12-05 17:03:54 +0900

    Install Flow type

--- src/js/low_level/rollup.config.js
+++ src/js/low_level/rollup.config.js
@@ -1,6 +1,9 @@
+import flow from 'rollup-plugin-flow';
+
 export default {
   entry: 'src/js/low_level/glboost_include.js',
   dest: 'build/glboost-low.js',
   format: 'umd',
+  plugins: [ flow() ],
   moduleName: 'GLBoost',
 }
