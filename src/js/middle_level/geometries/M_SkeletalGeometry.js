0c44dd0aca1982f9266816c3fea6593ded8445b6
Yuki Shimada <emadurandal@gmail.com>
2017-12-05 14:16:41 +0900

    Divide the processing of Skinning between desktop and mobile

--- src/js/middle_level/geometries/M_SkeletalGeometry.js
+++ src/js/middle_level/geometries/M_SkeletalGeometry.js
@@ -79,10 +79,7 @@ export default class M_SkeletalGeometry extends Geometry {
       joints[i].updateGizmoDisplay();
     }
 
-    /*
-    // experiment of jointmatrix -> quaternion and translation -> jointmatrix
-    for (let i=0; i<matrices.length; i++) {
-
+/*
       let s = matrices[i].getScale();
       
       matrices[i].m00 /= s.x;
@@ -114,117 +111,119 @@ export default class M_SkeletalGeometry extends Geometry {
       matrices[i] = matrix;
     }
     */
-    
-    let flatMatrices = [];
-    for (let i=0; i<matrices.length; i++) {
-      Array.prototype.push.apply(flatMatrices, matrices[i].flattenAsArray());
-    }
 
-    if (matrices.length < 4) {
-      let identityMatrices = [];
-      for (let i=0; i<(4 - matrices.length); i++) {
-        Array.prototype.push.apply(identityMatrices,
-          [1, 0, 0, 0,
-            0, 1, 0, 0,
-            0, 0, 1, 0,
-            0, 0, 0, 1]
-        );
-      }
-      Array.prototype.push.apply(flatMatrices, identityMatrices);
-    }
-    this._jointMatrices = flatMatrices;
-    
+    if (!GLBoost.VALUE_TARGET_IS_MOBILE) {
 
-    /*
-
-    {
-      // no comporess
-
-      this._qArray = new Float32Array(matrices.length * 4);
-      this._tArray = new Float32Array(matrices.length * 3);
-
+      let flatMatrices = [];
       for (let i=0; i<matrices.length; i++) {
-        let q = (Quaternion.quaternionFromRotationMatrix(matrices[i]));
-        q.normalize();
-        this._qArray[i*4+0] = q.x;
-        this._qArray[i*4+1] = q.y;
-        this._qArray[i*4+2] = q.z;
-        this._qArray[i*4+3] = q.w;
-        let t = matrices[i].getTranslate();
-        this._tArray[i*3+0] = t.x;
-        this._tArray[i*3+1] = t.y;
-        this._tArray[i*3+2] = t.z;
-      }
-    }
-    */
-/*
-    {
-      // comporess quaternion only
-
-      this._qArray = new Float32Array(matrices.length * 2);
-      this._tArray = new Float32Array(matrices.length * 3);
-
-      for (let i=0; i<matrices.length; i++) {
-        let q = (Quaternion.quaternionFromRotationMatrix(matrices[i]));
-        q.normalize();
-        let vec2QPacked = MathUtil.packNormalizedVec4ToVec2(q.x, q.y, q.z, q.w, 4096);
-        this._qArray[i*2+0] = vec2QPacked[0];
-        this._qArray[i*2+1] = vec2QPacked[1];
-        let t = matrices[i].getTranslate();
-        this._tArray[i*3+0] = t.x;
-        this._tArray[i*3+1] = t.y;
-        this._tArray[i*3+2] = t.z;
-      }
-    }
-*/
-/*
-    
-    {
-      // comporess both of quaternion and traslation
-
-      this._qtArray = new Float32Array(matrices.length * 4);
-      let tXArray = [];
-      let tYArray = [];
-      let tZArray = [];
-      for (let i=0; i<matrices.length; i++) {
-        let t = matrices[i].getTranslate();        
-        tXArray.push(Math.abs(t.x));
-        tYArray.push(Math.abs(t.y));
-        tZArray.push(Math.abs(t.z));
+        Array.prototype.push.apply(flatMatrices, matrices[i].flattenAsArray());
       }
 
-      let maxX = Math.max.apply(null, tXArray);
-      let maxY = Math.max.apply(null, tYArray);
-      let maxZ = Math.max.apply(null, tZArray);
-      this._translationScale.x = maxX*1.1;
-      this._translationScale.y = maxY*1.1;
-      this._translationScale.z = maxZ*1.1;
-
-      console.log('getScale are ...');
-      for (let i=0; i<matrices.length; i++) {
-        let s = matrices[i].getScale();
-        console.log(s.toString());
-
-
-        let q = (Quaternion.quaternionFromRotationMatrix(matrices[i]));
-        q.normalize();
-        let vec2QPacked = MathUtil.packNormalizedVec4ToVec2(q.x, q.y, q.z, q.w, 4096);
-        let t = matrices[i].getTranslate();
-        this._qtArray[i*4+0] = vec2QPacked[0];
-        this._qtArray[i*4+1] = vec2QPacked[1];
-        let vec2TPacked = MathUtil.packNormalizedVec4ToVec2(
-          t.x/this._translationScale.x, t.y/this._translationScale.y,
-          t.z/this._translationScale.z, 0.0, 4096);
-        this._qtArray[i*4+2] = vec2TPacked[0];
-        this._qtArray[i*4+3] = vec2TPacked[1];
+      if (matrices.length < 4) {
+        let identityMatrices = [];
+        for (let i=0; i<(4 - matrices.length); i++) {
+          Array.prototype.push.apply(identityMatrices,
+            [1, 0, 0, 0,
+              0, 1, 0, 0,
+              0, 0, 1, 0,
+              0, 0, 0, 1]
+          );
+        }
+        Array.prototype.push.apply(flatMatrices, identityMatrices);
       }
-    }
-    */
+      this._jointMatrices = flatMatrices;
+
+    } else {
+      /*
+
+      {
+        // no comporess
+
+        this._qArray = new Float32Array(matrices.length * 4);
+        this._tArray = new Float32Array(matrices.length * 3);
+
+        for (let i=0; i<matrices.length; i++) {
+          let q = (Quaternion.quaternionFromRotationMatrix(matrices[i]));
+          q.normalize();
+          this._qArray[i*4+0] = q.x;
+          this._qArray[i*4+1] = q.y;
+          this._qArray[i*4+2] = q.z;
+          this._qArray[i*4+3] = q.w;
+          let t = matrices[i].getTranslate();
+          this._tArray[i*3+0] = t.x;
+          this._tArray[i*3+1] = t.y;
+          this._tArray[i*3+2] = t.z;
+        }
+      }
+      */
+      /*
+      {
+        // comporess quaternion only
+
+        this._qArray = new Float32Array(matrices.length * 2);
+        this._tArray = new Float32Array(matrices.length * 3);
+
+        for (let i=0; i<matrices.length; i++) {
+          let q = (Quaternion.quaternionFromRotationMatrix(matrices[i]));
+          q.normalize();
+          let vec2QPacked = MathUtil.packNormalizedVec4ToVec2(q.x, q.y, q.z, q.w, 4096);
+          this._qArray[i*2+0] = vec2QPacked[0];
+          this._qArray[i*2+1] = vec2QPacked[1];
+          let t = matrices[i].getTranslate();
+          this._tArray[i*3+0] = t.x;
+          this._tArray[i*3+1] = t.y;
+          this._tArray[i*3+2] = t.z;
+        }
+      }
+      */
+
+      
+      {
+        // comporess both of quaternion and traslation
+
+        this._qtArray = new Float32Array(matrices.length * 4);
+        let tXArray = [];
+        let tYArray = [];
+        let tZArray = [];
+        for (let i=0; i<matrices.length; i++) {
+          let t = matrices[i].getTranslate();        
+          tXArray.push(Math.abs(t.x));
+          tYArray.push(Math.abs(t.y));
+          tZArray.push(Math.abs(t.z));
+        }
+
+        let maxX = Math.max.apply(null, tXArray);
+        let maxY = Math.max.apply(null, tYArray);
+        let maxZ = Math.max.apply(null, tZArray);
+        this._translationScale.x = maxX*1.1;
+        this._translationScale.y = maxY*1.1;
+        this._translationScale.z = maxZ*1.1;
+
+        // console.log('getScale are ...');
+        for (let i=0; i<matrices.length; i++) {
+          let s = matrices[i].getScale();
+          // console.log(s.toString());
+
+
+          let q = (Quaternion.quaternionFromRotationMatrix(matrices[i]));
+          q.normalize();
+          let vec2QPacked = MathUtil.packNormalizedVec4ToVec2(q.x, q.y, q.z, q.w, 4096);
+          let t = matrices[i].getTranslate();
+          this._qtArray[i*4+0] = vec2QPacked[0];
+          this._qtArray[i*4+1] = vec2QPacked[1];
+          let vec2TPacked = MathUtil.packNormalizedVec4ToVec2(
+            t.x/this._translationScale.x, t.y/this._translationScale.y,
+            t.z/this._translationScale.z, 0.0, 4096);
+          this._qtArray[i*4+2] = vec2TPacked[0];
+          this._qtArray[i*4+3] = vec2TPacked[1];
+        }
+      }
+    }    
 
   }
 
   draw(expression, lights, camera, skeletalMesh, scene, renderPass_index) {
-    if (this._jointMatrices === null) {
+    if (this._jointMatrices === null && this._qtArray === null) {
       return;
     }
     var gl = this._glContext.gl;
@@ -323,16 +322,16 @@ export default class M_SkeletalGeometry extends Geometry {
       var glslProgram = materials[i].shaderInstance.glslProgram;
       this._glContext.useProgram(glslProgram);
       
-      Shader.trySettingMatrix44ToUniform(gl, glslProgram, materials[i], materials[i]._semanticsDic, 'JOINTMATRIX', new Float32Array(this._jointMatrices));
-<<<<<<< Updated upstream
-=======
+      if (!GLBoost.VALUE_TARGET_IS_MOBILE) {
+        Shader.trySettingMatrix44ToUniform(gl, glslProgram, materials[i], materials[i]._semanticsDic, 'JOINTMATRIX', new Float32Array(this._jointMatrices));
+      } else {
  //     Shader.trySettingVec4ArrayToUniform(gl, glslProgram, materials[i], materials[i]._semanticsDic, 'JOINT_QUATERNION', this._qArray);
 //      Shader.trySettingVec2ArrayToUniform(gl, glslProgram, materials[i], materials[i]._semanticsDic, 'JOINT_QUATERNION', this._qArray);
  //     Shader.trySettingVec3ArrayToUniform(gl, glslProgram, materials[i], materials[i]._semanticsDic, 'JOINT_TRANSLATION', this._tArray);      
-//      Shader.trySettingVec4ArrayToUniform(gl, glslProgram, materials[i], materials[i]._semanticsDic, 'JOINT_QUATTRANSLATION', this._qtArray);//
-//      this._glContext.uniform3f(materials[i].getUniform(glslProgram, 'uniform_translationScale'),
-//        this._translationScale.x, this._translationScale.y, this._translationScale.z, true);
->>>>>>> Stashed changes
+        Shader.trySettingVec4ArrayToUniform(gl, glslProgram, materials[i], materials[i]._semanticsDic, 'JOINT_QUATTRANSLATION', this._qtArray);//
+        this._glContext.uniform3f(materials[i].getUniform(glslProgram, 'uniform_translationScale'),
+          this._translationScale.x, this._translationScale.y, this._translationScale.z, true);
+      }
     }
 
     super.draw(expression, lights, camera, skeletalMesh, scene, renderPass_index);
