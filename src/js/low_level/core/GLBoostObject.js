aa3357dcfdf675876772cf9a3483ac17a1b3a75a
Yuki Shimada <emadurandal@gmail.com>
2017-12-10 05:26:04 +0900

    Add Allocation Number setting

--- src/js/low_level/core/GLBoostObject.js
+++ src/js/low_level/core/GLBoostObject.js
@@ -1,4 +1,5 @@
 import L_GLBoostMonitor from './L_GLBoostMonitor';
+import GLBoost from '../../globals';
 
 export default class GLBoostObject {
   constructor(glBoostContext, toRegister = true) {
@@ -20,6 +21,59 @@ export default class GLBoostObject {
     this._classUniqueNumber = 0;
     for(let i=0; i<this.className.length; i++) {
       this._classUniqueNumber += this.className.charCodeAt(i);
+    }
+
+  }
+
+  setupExistIndexAndArray() {
+    this._objectIndex = -1;
+    this._materialIndex = -1;
+    this._lightIndex = -1;
+    this._jointSetIndex = -1;
+    this._morphIndex = -1;
+
+    const seekSpaceOfArrayAndSetIndexThere = (typeName)=>{
+      let array = GLBoostObject['_' + typeName + 'ExistArray'];
+      for (let i=0; i<array.length; i++) {
+        if (array[i] === void 0) {
+          this['_' + typeName + 'Index'] = i;
+          array[i] = true;
+          return;
+        }
+      }
+      this['_' + typeName + 'Index'] = array.length;
+      array[array.length] = true;
+    };
+
+    if (this.className.indexOf('Mesh') !== -1) {
+      seekSpaceOfArrayAndSetIndexThere('object');
+      if (this.className.indexOf('SkeltalMesh') !== -1) {
+        seekSpaceOfArrayAndSetIndexThere('jointSet');
+      }
+    } else if (this.className.indexOf('Light') !== -1) {
+      seekSpaceOfArrayAndSetIndexThere('light');
+    } else if (this.className.indexOf('Material') !== -1) {
+      seekSpaceOfArrayAndSetIndexThere('material');
+    }
+
+  }
+
+  tearDownExistIndexAndArray() {
+    const deleteIndex = (typeName)=>{
+      let array = GLBoostObject['_' + typeName + 'ExistArray'];
+      delete array[this['_' + typeName + 'Index']];
+      this['_' + typeName + 'Index'] = -1;
+    };
+
+    if (this.className.indexOf('Mesh') !== -1) {
+      deleteIndex('object');
+      if (this.className.indexOf('SkeltalMesh') !== -1) {
+        deleteIndex('jointSet');
+      }
+    } else if (this.className.indexOf('Light') !== -1) {
+      deleteIndex('light');
+    } else if (this.className.indexOf('Material') !== -1) {
+      deleteIndex('material');
     }
   }
 
@@ -75,6 +129,9 @@ export default class GLBoostObject {
     if (this._toRegister) {
       this._glBoostMonitor.deregisterGLBoostObject(this);
     }
+
+    this.tearDownExistIndexAndArray();
+
     this._readyForDiscard = true;
   }
 
@@ -88,3 +145,10 @@ export default class GLBoostObject {
 }
 
 GLBoostObject.classInfoDic = {};
+GLBoostObject._objectExistArray = [];
+GLBoostObject._materialExistArray = [];
+GLBoostObject._lightExistArray = [];
+GLBoostObject._jointSetExistArray = [];
+GLBoostObject._morphExistArray = [];
+
+GLBoost['GLBoostObject'] = GLBoostObject;
