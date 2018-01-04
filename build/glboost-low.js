7d9cc48081c2c2deabdc6549b661c30ead0fb615
Yuki Shimada <emadurandal@gmail.com>
2018-01-04 12:25:15 +0900

    Update distribution files

--- build/glboost-low.js
+++ build/glboost-low.js
@@ -5857,6 +5857,126 @@ class AABB {
 
 GLBoost$1['AABB'] = AABB;
 
+class FreeShader extends Shader {
+  constructor(glBoostContext, vertexShaderText, fragmentShaderText, attributes, uniforms, textureNames) {
+    super(glBoostContext);
+
+    this._vertexShaderText = vertexShaderText;
+    this._fragmentShaderText = fragmentShaderText;
+
+
+    let newAttributes = {};
+    for (let attributeName in attributes) {
+      switch (attributes[attributeName]) {
+        case 'POSITION':
+          newAttributes.position = attributeName;
+          break;
+        case 'NORMAL':
+          newAttributes.normal = attributeName;
+          break;
+        case 'COLOR':
+          newAttributes.color = attributeName;
+          break;
+        case 'TEXCOORD_0':
+          newAttributes.texcoord = attributeName;
+          break;
+        case 'JOINT':
+          newAttributes.joint = attributeName;
+          break;
+        case 'WEIGHT':
+          newAttributes.weight = attributeName;
+          break;
+        default:
+          newAttributes[attributes[attributeName]] = attributeName;
+          break;
+      }
+    }
+
+    this._attributes = newAttributes;
+    this._uniforms = uniforms;
+    this._textureNames = textureNames;
+  }
+
+  _getVertexShaderString(gl, functions, existCamera_f, lights, material, extraData) {
+    return this._vertexShaderText;
+  }
+
+  _getFragmentShaderString(gl, functions, lights, material, extraData) {
+    return this._fragmentShaderText;
+  }
+
+  _prepareAssetsForShaders(gl, shaderProgram, expression, vertexAttribs, existCamera_f, lights, material, extraData) {
+    var vertexAttribsAsResult = [];
+
+    vertexAttribs.forEach((attribName)=>{
+      shaderProgram['vertexAttribute_' + attribName] = gl.getAttribLocation(shaderProgram, this._attributes[attribName]);
+      if (shaderProgram['vertexAttribute_' + attribName] >= 0) {
+        gl.enableVertexAttribArray(shaderProgram['vertexAttribute_' + attribName]);
+        vertexAttribsAsResult.push(attribName);
+      }
+    });
+
+    let textureCount = 0;
+    this._glContext.useProgram(shaderProgram);
+
+    for (let uniformName in this._uniforms) {
+      if (this._uniforms[uniformName] === 'TEXTURE') {
+        material.uniformTextureSamplerDic[uniformName] = {};
+        let textureUniformLocation = this._glContext.getUniformLocation(shaderProgram, uniformName);
+        if (textureUniformLocation < 0) {
+          continue;
+        }
+        material.uniformTextureSamplerDic[uniformName].textureName = this._textureNames[uniformName];
+        material.uniformTextureSamplerDic[uniformName].textureUnitIndex = textureCount;
+
+        this._glContext.uniform1i(textureUniformLocation, textureCount, true);
+
+        textureCount++;
+      }
+
+      switch (this._uniforms[uniformName]) {
+        case 'MODELVIEW':
+        case 'MODELVIEWINVERSETRANSPOSE':
+        case 'PROJECTION':
+        case 'JOINTMATRIX':
+          material.setUniform(shaderProgram, 'uniform_' + uniformName, this._glContext.getUniformLocation(shaderProgram, uniformName));
+        case 'TEXTURE':
+          material.addSemanticsDic(this._uniforms[uniformName], uniformName);
+          continue;
+      }
+
+      material.setUniform(shaderProgram, 'uniform_' + uniformName, this._glContext.getUniformLocation(shaderProgram, uniformName));
+
+    }
+
+    return vertexAttribsAsResult;
+  }
+
+  get attributes() {
+    return this._attributes;
+  }
+
+  setUniforms(gl, glslProgram, expression, material, camera, mesh, lights) {
+    super.setUniforms(gl, glslProgram, expression, material, camera, mesh, lights);
+    
+    for (let uniformName in this._uniforms) {
+      let value = this._uniforms[uniformName];
+
+      if (typeof value === 'number') {
+        this._glContext.uniform1f(material.getUniform(glslProgram, 'uniform_' + uniformName), value, true);
+      } else if (value instanceof Vector2) {
+        this._glContext.uniform2f(material.getUniform(glslProgram, 'uniform_' + uniformName), value.x, value.y, true);
+      } else if (value instanceof Vector3) {
+        this._glContext.uniform3f(material.getUniform(glslProgram, 'uniform_' + uniformName), value.x, value.y, value.z, true);
+      } else if (value instanceof Vector4) {
+        this._glContext.uniform4f(material.getUniform(glslProgram, 'uniform_' + uniformName), value.x, value.y, value.z, value.w, true);
+      }
+    }
+  }
+}
+
+GLBoost['FreeShader'] = FreeShader;
+
 class Geometry extends GLBoostObject {
   constructor(glBoostContext) {
     super(glBoostContext);
@@ -6247,7 +6367,7 @@ class Geometry extends GLBoostObject {
     return Geometry._allVertexAttribs(this._vertices);
   } 
 
-  prepareGLSLProgramAndSetVertexNtoMaterial(expression, material, index, existCamera_f, lights, doSetupVertexAttribs = true, shaderClass = void 0) {
+  prepareGLSLProgramAndSetVertexNtoMaterial(expression, material, index, existCamera_f, lights, doSetupVertexAttribs = true, shaderClass = void 0, argShaderInstance = void 0) {
     let gl = this._glContext.gl;
     let vertices = this._vertices;
 
@@ -6258,13 +6378,16 @@ class Geometry extends GLBoostObject {
 //      glem.bindVertexArray(gl, Geometry._vaoDic[this.toString()]);
 //    }
 
-    
 
     let shaderInstance = null;
-    if (shaderClass) {
-      shaderInstance = Shader._createShaderInstance(this._glBoostContext, shaderClass);
+    if (argShaderInstance) {
+      shaderInstance = argShaderInstance;
     } else {
-      shaderInstance = Shader._createShaderInstance(this._glBoostContext, material.shaderClass);
+      if (shaderClass) {
+        shaderInstance = Shader._createShaderInstance(this._glBoostContext, shaderClass);
+      } else {
+        shaderInstance = Shader._createShaderInstance(this._glBoostContext, material.shaderClass);
+      }  
     }
 
     let glslProgram = shaderInstance.getShaderProgram(expression, _optimizedVertexAttribs, existCamera_f, lights, material, this._extraDataForShader);
@@ -6381,7 +6504,12 @@ class Geometry extends GLBoostObject {
     //let materials = this._getAppropriateMaterials(mesh);
 
     for (let i=0; i<materials.length;i++) {
-      let shaderInstance = this.prepareGLSLProgramAndSetVertexNtoMaterial(expression, materials[i], i, existCamera_f, lights, doAfter, shaderClass);
+      let shaderInstance = null;
+      if (materials[i].shaderInstance && materials[i].shaderInstance.constructor === FreeShader) {
+        shaderInstance = this.prepareGLSLProgramAndSetVertexNtoMaterial(expression, materials[i], i, existCamera_f, lights, doAfter, void 0, materials[i].shaderInstance);
+      } else {
+        shaderInstance = this.prepareGLSLProgramAndSetVertexNtoMaterial(expression, materials[i], i, existCamera_f, lights, doAfter, shaderClass);
+      }
       this._setVertexNtoSingleMaterial(materials[i], i);
       shaderInstance.vao = Geometry._vaoDic[this.toString()];
       this.setUpVertexAttribs(gl, shaderInstance._glslProgram, allVertexAttribs);
