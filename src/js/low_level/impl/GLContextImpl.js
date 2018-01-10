ede8ec14566005a77827191da8e4a9e4b5da2f22
Yuki Shimada <emadurandal@gmail.com>
2018-01-10 18:59:48 +0900

    Make premultipliedAlpha false

--- src/js/low_level/impl/GLContextImpl.js
+++ src/js/low_level/impl/GLContextImpl.js
@@ -26,7 +26,7 @@ export default class GLContextImpl {
       this._gl = gl;
     } else {
 
-      let gl = this._canvas.getContext(glVersionString, { antialias: true });
+      let gl = this._canvas.getContext(glVersionString, { antialias: true, premultipliedAlpha: false });
 
       if (!gl) {
         gl = this._canvas.getContext('experimental-' + glVersionString);
