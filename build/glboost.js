6522801c6e75b73af7db0e13f5fbb51924f40af1
Yuki Shimada <emadurandal@gmail.com>
2018-05-04 01:06:11 +0900

    Add Implementation to SPVGLTFLoaderExtension

--- build/glboost.js
+++ build/glboost.js
@@ -4,7 +4,7 @@
 	(factory());
 }(this, (function () { 'use strict';
 
-// This revision is the commit right after the SHA: eabbcca1
+// This revision is the commit right after the SHA: 288128fa
 var global = ('global',eval)('this');
 
 (function (global) {
@@ -17168,6 +17168,11 @@ class GLTFLoader {
         'TEXTURE_WRAP_T': samplerJson.wrapT,
         'UNPACK_PREMULTIPLY_ALPHA_WEBGL': isNeededToMultiplyAlphaToColorOfTexture
       });
+      
+      if (options.extensionLoader && options.extensionLoader.setUVTransformToTexture) {
+        options.extensionLoader.setUVTransformToTexture(texture, samplerJson);
+      }
+
       let promise = texture.generateTextureFromUri(textureUri, false);
       textures[textureName] = texture;
       promisesToLoadResources.push(promise);
@@ -17222,42 +17227,12 @@ class GLTFLoader {
       // Animation
       this._loadAnimation(group, buffers, json, glTFVer);
 
-      if (options.extensionLoader) {
+      if (options.extensionLoader && options.extensionLoader.setAssetPropertiesToRootGroup) {
         options.extensionLoader.setAssetPropertiesToRootGroup(rootGroup, json.asset);
       }
 
       rootGroup.addChild(group);
-      
-/*
-      rootGroup.asset = json.asset;
 
-      // Animation FPS
-      if (json.asset && json.asset.animationFps) {
-        rootGroup.animationFps = json.asset.animationFps;
-      }
-
-      // Animation Tracks
-      if (json.asset && json.asset.extras && json.asset.extras.animation_tracks) {
-        rootGroup.animationTracks = json.asset.extras.animation_tracks;
-      }
-
-      rootGroup.addChild(group);
-
-      // Transparent Meshes Draw Order
-      if (json.asset && json.asset.extras && json.asset.extras.transparent_meshes_draw_order) {
-        rootGroup.transparentMeshesDrawOrder = json.asset.extras.transparent_meshes_draw_order;
-        let meshes = rootGroup.searchElementsByType(M_Mesh);
-        rootGroup.transparentMeshes = [];
-        for (let name of rootGroup.transparentMeshesDrawOrder) {
-          for (let mesh of meshes) {
-            if (mesh.userFlavorName === name) {
-              rootGroup.transparentMeshes.push(mesh);
-              break;
-            }
-          }
-        }
-      }
-      */
     }
 
     resolve(rootGroup);
--- src/js/middle_level/loader/SPVGLTFLoaderExtension.js
+++ src/js/middle_level/loader/SPVGLTFLoaderExtension.js
@@ -32,5 +32,39 @@ export default class SPVGLTFLoaderExtension {
     if (asset && asset.animationFps) {
       rootGroup.animationFps = asset.animationFps;
     }
+
+    // other information
+    if (json.asset && json.asset.spv_version) {
+      rootGroup.spv_version = json.asset.spv_version;
+    }
+    if (json.asset && json.asset.LastSaved_ApplicationVendor) {
+      rootGroup.LastSaved_ApplicationVendor = json.asset.LastSaved_ApplicationVendor;
+    }
+    if (json.asset && json.asset.LastSaved_ApplicationName) {
+      rootGroup.LastSaved_ApplicationName = json.asset.LastSaved_ApplicationName;
+    }
+    if (json.asset && json.asset.LastSaved_ApplicationVersion) {
+      rootGroup.LastSaved_ApplicationVersion = json.asset.LastSaved_ApplicationVersion;
+    }
+
+    // Animation Tracks
+    if (json.asset && json.asset.extras && json.asset.extras.animation_tracks) {
+      rootGroup.animationTracks = json.asset.extras.animation_tracks;
+    }
+  }
+
+  setUVTransformToTexture(texture, samplerJson) {
+    let uvTransform = new Vector4(1, 1, 0, 0);
+    if (samplerJson.extras && samplerJson.extras.scale) {
+      let scale = samplerJson.extras.scale;
+      uvTransform.x = scale[0];
+      uvTransform.y = scale[1];
+    }
+    if (samplerJson.extras && samplerJson.extras.translation) {
+      let translation = samplerJson.extras.translation;
+      uvTransform.z = translation[0];
+      uvTransform.w = translation[1];
+    }
+    texture.uvTransform = uvTransform;
   }
 }
