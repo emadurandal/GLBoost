de57974bbb3db8389554b0366abd6a5031c449a5
Yuki Shimada <emadurandal@gmail.com>
2018-05-06 06:19:52 +0900

    Make a canvas of a example transparent

--- build/glboost.js
+++ build/glboost.js
@@ -4,7 +4,7 @@
 	(factory());
 }(this, (function () { 'use strict';
 
-// This revision is the commit right after the SHA: b3d0deec
+// This revision is the commit right after the SHA: 655ade1b
 var global = ('global',eval)('this');
 
 (function (global) {
