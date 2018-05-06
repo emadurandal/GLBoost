446cdf0fe7922a343bf63a3526c20fdef9a722a0
Yuki Shimada <emadurandal@gmail.com>
2018-05-06 13:21:17 +0900

    [WIP]

--- build/glboost.js
+++ build/glboost.js
@@ -4,7 +4,7 @@
 	(factory());
 }(this, (function () { 'use strict';
 
-// This revision is the commit right after the SHA: 96de9ab5
+// This revision is the commit right after the SHA: f1e436f6
 var global = ('global',eval)('this');
 
 (function (global) {
@@ -17285,7 +17285,48 @@ class ModelConverter {
       this._accessBinaryWithAccessor(accessor);
     }
 
+    this._setupMeshGeometries(gltfModel);
+
     return gltfModel;
+  }
+
+  _setupMeshGeometries(gltfModel) {
+    for (let mesh of gltfModel.meshes) {
+
+      let _indicesArray = [];
+      let _positions = [];
+      let _normals = [];
+      let vertexData = {
+        position: _positions,
+        normal: _normals,
+        components: {},
+        componentBytes: {},
+        componentType: {}
+      };
+      let additional = {
+        'joint': [],
+        'weight': [],
+        'texcoord': []
+      };
+
+      let dataViewMethodDic = {};
+
+      for (let i in mesh.primitives) {
+        let primitive = mesh.primitives[i];
+        for (let attributeName in primitive.attributes) {
+          let accessor =  primitive.attributes[attributeName];
+
+          if (attributeName === 'POSITION') {
+            _positions[i] = accessor.extras.vertexAttributeArray;
+            vertexData.components.position = accessor.extras.componentN;
+            vertexData.componentBytes.position = accessor.extras.componentBytes;
+            vertexData.componentType.position = accessor.componentType;
+            dataViewMethodDic.position = accessor.extras.dataViewMethod;
+          }
+
+        }
+      }
+    }
   }
 
   _adjustByteAlign(typedArrayClass, arrayBuffer, alignSize, byteOffset, length) {
@@ -17388,35 +17429,41 @@ class ModelConverter {
     var arrayBuffer = accessor.buffer.buffer;
 
     let componentN = this._checkComponentNumber(accessor);
-    let bytesPerComponent = this._checkBytesPerComponent(accessor);
-    var dataViewMethod = this._checkDataViewMethod(accessor);
+    let componentBytes = this._checkBytesPerComponent(accessor);
+    let dataViewMethod = this._checkDataViewMethod(accessor);
+    if (accessor.extras) {
+      accessor.extras = {};
+    }
 
+    accessor.extras.componentN = componentN;
+    accessor.extras.componentBytes = componentBytes;
+    accessor.extras.dataViewMethod = dataViewMethod;
 
-    var byteLength = bytesPerComponent * componentN * accessor.count;
+    var byteLength = componentBytes * componentN * accessor.count;
 
     var vertexAttributeArray = [];
 
     if (accessor.extras && accessor.extras.toGetAsTypedArray) {
       if (GLTFLoader._isSystemLittleEndian()) {
         if (dataViewMethod === 'getFloat32') {
-          vertexAttributeArray = this._adjustByteAlign(Float32Array, arrayBuffer, 4, byteOffset, byteLength / bytesPerComponent);
+          vertexAttributeArray = this._adjustByteAlign(Float32Array, arrayBuffer, 4, byteOffset, byteLength / componentBytes);
         } else if (dataViewMethod === 'getInt8') {
-          vertexAttributeArray = new Int8Array(arrayBuffer, byteOffset, byteLength / bytesPerComponent);
+          vertexAttributeArray = new Int8Array(arrayBuffer, byteOffset, byteLength / componentBytes);
         } else if (dataViewMethod === 'getUint8') {
-          vertexAttributeArray = new Uint8Array(arrayBuffer, byteOffset, byteLength / bytesPerComponent);
+          vertexAttributeArray = new Uint8Array(arrayBuffer, byteOffset, byteLength / componentBytes);
         } else if (dataViewMethod === 'getInt16') {
-          vertexAttributeArray = this._adjustByteAlign(Int16Array, arrayBuffer, 2, byteOffset, byteLength / bytesPerComponent);
+          vertexAttributeArray = this._adjustByteAlign(Int16Array, arrayBuffer, 2, byteOffset, byteLength / componentBytes);
         } else if (dataViewMethod === 'getUint16') {
-          vertexAttributeArray = this._adjustByteAlign(Uint16Array, arrayBuffer, 2, byteOffset, byteLength / bytesPerComponent);
+          vertexAttributeArray = this._adjustByteAlign(Uint16Array, arrayBuffer, 2, byteOffset, byteLength / componentBytes);
         } else if (dataViewMethod === 'getInt32') {
-          vertexAttributeArray = this._adjustByteAlign(Int32Array, arrayBuffer, 4, byteOffset, byteLength / bytesPerComponent);
+          vertexAttributeArray = this._adjustByteAlign(Int32Array, arrayBuffer, 4, byteOffset, byteLength / componentBytes);
         } else if (dataViewMethod === 'getUint32') {
-          vertexAttributeArray = this._adjustByteAlign(Uint32Array, arrayBuffer, 4, byteOffset, byteLength / bytesPerComponent);
+          vertexAttributeArray = this._adjustByteAlign(Uint32Array, arrayBuffer, 4, byteOffset, byteLength / componentBytes);
         }
 
       } else {
         let dataView = new DataView(arrayBuffer, byteOffset, byteLength);
-        let byteDelta = bytesPerComponent * componentN;
+        let byteDelta = componentBytes * componentN;
         let littleEndian = true;
         for (let pos = 0; pos < byteLength; pos += byteDelta) {
           switch (accessor.type) {
@@ -17425,18 +17472,18 @@ class ModelConverter {
               break;
             case 'VEC2':
               vertexAttributeArray.push(dataView[dataViewMethod](pos, littleEndian));
-              vertexAttributeArray.push(dataView[dataViewMethod](pos + bytesPerComponent, littleEndian));
+              vertexAttributeArray.push(dataView[dataViewMethod](pos + componentBytes, littleEndian));
               break;
             case 'VEC3':
               vertexAttributeArray.push(dataView[dataViewMethod](pos, littleEndian));
-              vertexAttributeArray.push(dataView[dataViewMethod](pos + bytesPerComponent, littleEndian));
-              vertexAttributeArray.push(dataView[dataViewMethod](pos + bytesPerComponent * 2, littleEndian));
+              vertexAttributeArray.push(dataView[dataViewMethod](pos + componentBytes, littleEndian));
+              vertexAttributeArray.push(dataView[dataViewMethod](pos + componentBytes * 2, littleEndian));
               break;
             case 'VEC4':
               vertexAttributeArray.push(dataView[dataViewMethod](pos, littleEndian));
-              vertexAttributeArray.push(dataView[dataViewMethod](pos + bytesPerComponent, littleEndian));
-              vertexAttributeArray.push(dataView[dataViewMethod](pos + bytesPerComponent * 2, littleEndian));
-              vertexAttributeArray.push(dataView[dataViewMethod](pos + bytesPerComponent * 3, littleEndian));
+              vertexAttributeArray.push(dataView[dataViewMethod](pos + componentBytes, littleEndian));
+              vertexAttributeArray.push(dataView[dataViewMethod](pos + componentBytes * 2, littleEndian));
+              vertexAttributeArray.push(dataView[dataViewMethod](pos + componentBytes * 3, littleEndian));
               break;
           }
         }
@@ -17458,7 +17505,7 @@ class ModelConverter {
       }
     } else {
       let dataView = new DataView(arrayBuffer, byteOffset, byteLength);
-      let byteDelta = bytesPerComponent * componentN;
+      let byteDelta = componentBytes * componentN;
       let littleEndian = true;
       for (let pos = 0; pos < byteLength; pos += byteDelta) {
 
@@ -17469,37 +17516,37 @@ class ModelConverter {
           case 'VEC2':
             vertexAttributeArray.push(new Vector2(
               dataView[dataViewMethod](pos, littleEndian),
-              dataView[dataViewMethod](pos+bytesPerComponent, littleEndian)
+              dataView[dataViewMethod](pos+componentBytes, littleEndian)
             ));
             break;
           case 'VEC3':
             vertexAttributeArray.push(new Vector3(
               dataView[dataViewMethod](pos, littleEndian),
-              dataView[dataViewMethod](pos+bytesPerComponent, littleEndian),
-              dataView[dataViewMethod](pos+bytesPerComponent*2, littleEndian)
+              dataView[dataViewMethod](pos+componentBytes, littleEndian),
+              dataView[dataViewMethod](pos+componentBytes*2, littleEndian)
             ));
             break;
           case 'VEC4':
             if (accessor.extras && accessor.extras.quaternionIfVec4) {
               vertexAttributeArray.push(new Quaternion(
                 dataView[dataViewMethod](pos, littleEndian),
-                dataView[dataViewMethod](pos+bytesPerComponent, littleEndian),
-                dataView[dataViewMethod](pos+bytesPerComponent*2, littleEndian),
-                dataView[dataViewMethod](pos+bytesPerComponent*3, littleEndian)
+                dataView[dataViewMethod](pos+componentBytes, littleEndian),
+                dataView[dataViewMethod](pos+componentBytes*2, littleEndian),
+                dataView[dataViewMethod](pos+componentBytes*3, littleEndian)
               ));
             } else {
               vertexAttributeArray.push(new Vector4(
                 dataView[dataViewMethod](pos, littleEndian),
-                dataView[dataViewMethod](pos+bytesPerComponent, littleEndian),
-                dataView[dataViewMethod](pos+bytesPerComponent*2, littleEndian),
-                dataView[dataViewMethod](pos+bytesPerComponent*3, littleEndian)
+                dataView[dataViewMethod](pos+componentBytes, littleEndian),
+                dataView[dataViewMethod](pos+componentBytes*2, littleEndian),
+                dataView[dataViewMethod](pos+componentBytes*3, littleEndian)
               ));
             }
             break;
           case 'MAT4':
             let matrixComponents = [];
             for (let i=0; i<16; i++) {
-              matrixComponents[i] = dataView[dataViewMethod](pos+bytesPerComponent*i, littleEndian);
+              matrixComponents[i] = dataView[dataViewMethod](pos+componentBytes*i, littleEndian);
             }
             vertexAttributeArray.push(new Matrix44(matrixComponents, true));
             break;
@@ -17508,6 +17555,7 @@ class ModelConverter {
       }
     }
 
+    accessor.extras.vertexAttributeArray = vertexAttributeArray;
 
     return vertexAttributeArray;
   }
--- src/js/middle_level/loader/ModelConverter.js
+++ src/js/middle_level/loader/ModelConverter.js
@@ -37,7 +37,48 @@ export default class ModelConverter {
       this._accessBinaryWithAccessor(accessor)
     }
 
+    this._setupMeshGeometries(gltfModel);
+
     return gltfModel;
+  }
+
+  _setupMeshGeometries(gltfModel) {
+    for (let mesh of gltfModel.meshes) {
+
+      let _indicesArray = [];
+      let _positions = [];
+      let _normals = [];
+      let vertexData = {
+        position: _positions,
+        normal: _normals,
+        components: {},
+        componentBytes: {},
+        componentType: {}
+      };
+      let additional = {
+        'joint': [],
+        'weight': [],
+        'texcoord': []
+      };
+
+      let dataViewMethodDic = {};
+
+      for (let i in mesh.primitives) {
+        let primitive = mesh.primitives[i];
+        for (let attributeName in primitive.attributes) {
+          let accessor =  primitive.attributes[attributeName];
+
+          if (attributeName === 'POSITION') {
+            _positions[i] = accessor.extras.vertexAttributeArray;
+            vertexData.components.position = accessor.extras.componentN;
+            vertexData.componentBytes.position = accessor.extras.componentBytes;
+            vertexData.componentType.position = accessor.componentType;
+            dataViewMethodDic.position = accessor.extras.dataViewMethod;
+          }
+
+        }
+      }
+    }
   }
 
   _adjustByteAlign(typedArrayClass, arrayBuffer, alignSize, byteOffset, length) {
@@ -140,35 +181,41 @@ export default class ModelConverter {
     var arrayBuffer = accessor.buffer.buffer;
 
     let componentN = this._checkComponentNumber(accessor);
-    let bytesPerComponent = this._checkBytesPerComponent(accessor);
-    var dataViewMethod = this._checkDataViewMethod(accessor);
+    let componentBytes = this._checkBytesPerComponent(accessor);
+    let dataViewMethod = this._checkDataViewMethod(accessor);
+    if (accessor.extras) {
+      accessor.extras = {};
+    }
 
+    accessor.extras.componentN = componentN;
+    accessor.extras.componentBytes = componentBytes;
+    accessor.extras.dataViewMethod = dataViewMethod;
 
-    var byteLength = bytesPerComponent * componentN * accessor.count;
+    var byteLength = componentBytes * componentN * accessor.count;
 
     var vertexAttributeArray = [];
 
     if (accessor.extras && accessor.extras.toGetAsTypedArray) {
       if (GLTFLoader._isSystemLittleEndian()) {
         if (dataViewMethod === 'getFloat32') {
-          vertexAttributeArray = this._adjustByteAlign(Float32Array, arrayBuffer, 4, byteOffset, byteLength / bytesPerComponent);
+          vertexAttributeArray = this._adjustByteAlign(Float32Array, arrayBuffer, 4, byteOffset, byteLength / componentBytes);
         } else if (dataViewMethod === 'getInt8') {
-          vertexAttributeArray = new Int8Array(arrayBuffer, byteOffset, byteLength / bytesPerComponent);
+          vertexAttributeArray = new Int8Array(arrayBuffer, byteOffset, byteLength / componentBytes);
         } else if (dataViewMethod === 'getUint8') {
-          vertexAttributeArray = new Uint8Array(arrayBuffer, byteOffset, byteLength / bytesPerComponent);
+          vertexAttributeArray = new Uint8Array(arrayBuffer, byteOffset, byteLength / componentBytes);
         } else if (dataViewMethod === 'getInt16') {
-          vertexAttributeArray = this._adjustByteAlign(Int16Array, arrayBuffer, 2, byteOffset, byteLength / bytesPerComponent);
+          vertexAttributeArray = this._adjustByteAlign(Int16Array, arrayBuffer, 2, byteOffset, byteLength / componentBytes);
         } else if (dataViewMethod === 'getUint16') {
-          vertexAttributeArray = this._adjustByteAlign(Uint16Array, arrayBuffer, 2, byteOffset, byteLength / bytesPerComponent);
+          vertexAttributeArray = this._adjustByteAlign(Uint16Array, arrayBuffer, 2, byteOffset, byteLength / componentBytes);
         } else if (dataViewMethod === 'getInt32') {
-          vertexAttributeArray = this._adjustByteAlign(Int32Array, arrayBuffer, 4, byteOffset, byteLength / bytesPerComponent);
+          vertexAttributeArray = this._adjustByteAlign(Int32Array, arrayBuffer, 4, byteOffset, byteLength / componentBytes);
         } else if (dataViewMethod === 'getUint32') {
-          vertexAttributeArray = this._adjustByteAlign(Uint32Array, arrayBuffer, 4, byteOffset, byteLength / bytesPerComponent);
+          vertexAttributeArray = this._adjustByteAlign(Uint32Array, arrayBuffer, 4, byteOffset, byteLength / componentBytes);
         }
 
       } else {
         let dataView = new DataView(arrayBuffer, byteOffset, byteLength);
-        let byteDelta = bytesPerComponent * componentN;
+        let byteDelta = componentBytes * componentN;
         let littleEndian = true;
         for (let pos = 0; pos < byteLength; pos += byteDelta) {
           switch (accessor.type) {
@@ -177,18 +224,18 @@ export default class ModelConverter {
               break;
             case 'VEC2':
               vertexAttributeArray.push(dataView[dataViewMethod](pos, littleEndian));
-              vertexAttributeArray.push(dataView[dataViewMethod](pos + bytesPerComponent, littleEndian));
+              vertexAttributeArray.push(dataView[dataViewMethod](pos + componentBytes, littleEndian));
               break;
             case 'VEC3':
               vertexAttributeArray.push(dataView[dataViewMethod](pos, littleEndian));
-              vertexAttributeArray.push(dataView[dataViewMethod](pos + bytesPerComponent, littleEndian));
-              vertexAttributeArray.push(dataView[dataViewMethod](pos + bytesPerComponent * 2, littleEndian));
+              vertexAttributeArray.push(dataView[dataViewMethod](pos + componentBytes, littleEndian));
+              vertexAttributeArray.push(dataView[dataViewMethod](pos + componentBytes * 2, littleEndian));
               break;
             case 'VEC4':
               vertexAttributeArray.push(dataView[dataViewMethod](pos, littleEndian));
-              vertexAttributeArray.push(dataView[dataViewMethod](pos + bytesPerComponent, littleEndian));
-              vertexAttributeArray.push(dataView[dataViewMethod](pos + bytesPerComponent * 2, littleEndian));
-              vertexAttributeArray.push(dataView[dataViewMethod](pos + bytesPerComponent * 3, littleEndian));
+              vertexAttributeArray.push(dataView[dataViewMethod](pos + componentBytes, littleEndian));
+              vertexAttributeArray.push(dataView[dataViewMethod](pos + componentBytes * 2, littleEndian));
+              vertexAttributeArray.push(dataView[dataViewMethod](pos + componentBytes * 3, littleEndian));
               break;
           }
         }
@@ -210,7 +257,7 @@ export default class ModelConverter {
       }
     } else {
       let dataView = new DataView(arrayBuffer, byteOffset, byteLength);
-      let byteDelta = bytesPerComponent * componentN;
+      let byteDelta = componentBytes * componentN;
       let littleEndian = true;
       for (let pos = 0; pos < byteLength; pos += byteDelta) {
 
@@ -221,37 +268,37 @@ export default class ModelConverter {
           case 'VEC2':
             vertexAttributeArray.push(new Vector2(
               dataView[dataViewMethod](pos, littleEndian),
-              dataView[dataViewMethod](pos+bytesPerComponent, littleEndian)
+              dataView[dataViewMethod](pos+componentBytes, littleEndian)
             ));
             break;
           case 'VEC3':
             vertexAttributeArray.push(new Vector3(
               dataView[dataViewMethod](pos, littleEndian),
-              dataView[dataViewMethod](pos+bytesPerComponent, littleEndian),
-              dataView[dataViewMethod](pos+bytesPerComponent*2, littleEndian)
+              dataView[dataViewMethod](pos+componentBytes, littleEndian),
+              dataView[dataViewMethod](pos+componentBytes*2, littleEndian)
             ));
             break;
           case 'VEC4':
             if (accessor.extras && accessor.extras.quaternionIfVec4) {
               vertexAttributeArray.push(new Quaternion(
                 dataView[dataViewMethod](pos, littleEndian),
-                dataView[dataViewMethod](pos+bytesPerComponent, littleEndian),
-                dataView[dataViewMethod](pos+bytesPerComponent*2, littleEndian),
-                dataView[dataViewMethod](pos+bytesPerComponent*3, littleEndian)
+                dataView[dataViewMethod](pos+componentBytes, littleEndian),
+                dataView[dataViewMethod](pos+componentBytes*2, littleEndian),
+                dataView[dataViewMethod](pos+componentBytes*3, littleEndian)
               ));
             } else {
               vertexAttributeArray.push(new Vector4(
                 dataView[dataViewMethod](pos, littleEndian),
-                dataView[dataViewMethod](pos+bytesPerComponent, littleEndian),
-                dataView[dataViewMethod](pos+bytesPerComponent*2, littleEndian),
-                dataView[dataViewMethod](pos+bytesPerComponent*3, littleEndian)
+                dataView[dataViewMethod](pos+componentBytes, littleEndian),
+                dataView[dataViewMethod](pos+componentBytes*2, littleEndian),
+                dataView[dataViewMethod](pos+componentBytes*3, littleEndian)
               ));
             }
             break;
           case 'MAT4':
             let matrixComponents = [];
             for (let i=0; i<16; i++) {
-              matrixComponents[i] = dataView[dataViewMethod](pos+bytesPerComponent*i, littleEndian);
+              matrixComponents[i] = dataView[dataViewMethod](pos+componentBytes*i, littleEndian);
             }
             vertexAttributeArray.push(new Matrix44(matrixComponents, true));
             break;
@@ -260,6 +307,7 @@ export default class ModelConverter {
       }
     }
 
+    accessor.extras.vertexAttributeArray = vertexAttributeArray;
 
     return vertexAttributeArray;
   }
