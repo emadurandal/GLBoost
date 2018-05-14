04818e183ef23391d0f9922d9d7de2926b2bde25
Yuki Shimada <emadurandal@gmail.com>
2018-05-15 00:12:27 +0900

    Fix Examples

--- build/glboost.js
+++ build/glboost.js
@@ -4,7 +4,7 @@
 	(factory());
 }(this, (function () { 'use strict';
 
-// This revision is the commit right after the SHA: 2b639e52
+// This revision is the commit right after the SHA: c62bc3ce
 var global = ('global',eval)('this');
 
 (function (global) {
