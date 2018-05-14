2676d8b24f223e96ccec097e825213e7f0d49d31
Yuki Shimada <emadurandal@gmail.com>
2018-05-15 08:10:39 +0900

    Fix addChild method of Group

--- build/glboost.js
+++ build/glboost.js
@@ -4,7 +4,7 @@
 	(factory());
 }(this, (function () { 'use strict';
 
-// This revision is the commit right after the SHA: 7ccaa3f5
+// This revision is the commit right after the SHA: 765e5c68
 var global = ('global',eval)('this');
 
 (function (global) {
