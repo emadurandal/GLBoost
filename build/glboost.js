84e078e790cb58e5689baa71fb19d593f3f97adc
Yuki Shimada <emadurandal@gmail.com>
2018-05-15 23:39:08 +0900

    Remove glTF1.1 sample

--- build/glboost.js
+++ build/glboost.js
@@ -4,7 +4,7 @@
 	(factory());
 }(this, (function () { 'use strict';
 
-// This revision is the commit right after the SHA: 7e94c89e
+// This revision is the commit right after the SHA: 9b330dfa
 var global = ('global',eval)('this');
 
 (function (global) {
