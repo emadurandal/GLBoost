34152106a360a855625bb4099ff260b7bb970ead
Yuki Shimada <emadurandal@gmail.com>
2018-05-25 11:22:30 +0900

    Fix to support WebVR in loading_gltf_skinning_anim sample

--- build/glboost.js
+++ build/glboost.js
@@ -4,7 +4,7 @@
   (factory());
 }(this, (function () { 'use strict';
 
-  // This revision is the commit right after the SHA: e4a5bded
+  // This revision is the commit right after the SHA: bbc3ad71
   var global = (0, eval)('this');
 
   (function (global) {
