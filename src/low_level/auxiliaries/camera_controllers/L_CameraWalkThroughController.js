3ed7dd813ea21e71ed5c811169b373eef85e8dc3
Unknown <emadurandal@gmail.com>
2018-06-17 11:17:56 +0900

    Add L_CameraWalkThroughController.js

---
+++ src/low_level/auxiliaries/camera_controllers/L_CameraWalkThroughController.js
@@ -0,0 +1,9 @@
+import GLBoostObject from '../../core/GLBoostObject';
+
+export default class L_CameraWalkThroughController extends GLBoostObject {
+  constructor(glBoostContext, options = {})
+  {
+    super(glBoostContext);
+
+  }
+}
