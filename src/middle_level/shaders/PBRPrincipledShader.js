707498018136646e9120917fb937bb6667badfee
Yuki Shimada <emadurandal@gmail.com>
2018-08-17 03:03:19 +0900

    Add PBRPrincipledShader.js

---
+++ src/middle_level/shaders/PBRPrincipledShader.js
@@ -0,0 +1,87 @@
+import Shader from '../../low_level/shaders/Shader';
+import DecalShader from './DecalShader';
+import Vector4 from '../../low_level/math/Vector4';
+
+export class PBRPrincipledShaderSource {
+
+  FSDefine_PBRPrincipledShaderSource(in_, f, lights) {
+    
+    var sampler2D = this._sampler2DShadow_func();
+    var shaderText = '';
+    shaderText += 'uniform vec4 Kd;\n';
+    shaderText += 'uniform vec4 ambient;\n'; // Ka * amount of ambient lights
+
+    let lightNumExceptAmbient = lights.filter((light)=>{return !light.isTypeAmbient();}).length;
+    if (lightNumExceptAmbient > 0) {
+      shaderText += `uniform highp ${sampler2D} uDepthTexture[${lightNumExceptAmbient}];\n`;
+      shaderText += `uniform int isShadowCasting[${lightNumExceptAmbient}];\n`;
+    }
+
+    return shaderText;
+  }
+
+  VSMethodDefine_PBRPrincipledShaderSource(f, lights, material, extraData) {
+    let shaderText = '';
+    shaderText += `
+    float foo(float val) {
+      return val;
+    }
+    `;
+
+    return shaderText;
+  }
+
+  FSShade_PBRPrincipledShaderSource(f, gl, lights) {
+    var shaderText = '';
+
+    shaderText += '  vec4 surfaceColor = rt0;\n';
+    shaderText += '  rt0 = vec4(0.0, 0.0, 0.0, 0.0);\n';
+    
+    for (let i=0; i<lights.length; i++) {
+      let light = lights[i];
+      let isShadowEnabledAsTexture = (light.camera && light.camera.texture) ? true:false;
+      shaderText += `  {\n`;
+      shaderText +=      Shader._generateLightStr(i);
+      shaderText +=      Shader._generateShadowingStr(gl, i, isShadowEnabledAsTexture);
+      shaderText += `    float diffuse = max(dot(lightDirection, normal), 0.0);\n`;
+      shaderText += `    rt0 += spotEffect * vec4(visibility, visibility, visibility, 1.0) * Kd * lightDiffuse[${i}] * vec4(diffuse, diffuse, diffuse, 1.0) * surfaceColor;\n`;
+      shaderText += `  }\n`;
+    }
+    shaderText += '  rt0.xyz += ambient.xyz;\n';
+    
+    //shaderText += '  rt0.a = 1.0;\n';
+    // shaderText += '  rt0 = surfaceColor;\n';
+//    shaderText += '  rt0 = vec4(v_shadowCoord[0].xy, 0.0, 1.0);\n';
+
+
+
+    return shaderText;
+  }
+
+  prepare_PBRPrincipledShaderSource(gl, shaderProgram, expression, vertexAttribs, existCamera_f, lights, material, extraData) {
+
+    var vertexAttribsAsResult = [];
+
+    material.setUniform(shaderProgram, 'uniform_Kd', this._glContext.getUniformLocation(shaderProgram, 'Kd'));
+    material.setUniform(shaderProgram, 'uniform_ambient', this._glContext.getUniformLocation(shaderProgram, 'ambient'));
+    
+    return vertexAttribsAsResult;
+  }
+}
+
+
+export default class PBRPrincipledShader extends DecalShader {
+  constructor(glBoostContext, basicShader) {
+
+    super(glBoostContext, basicShader);
+    LambertShader.mixin(LambertShaderSource);
+  }
+
+  setUniforms(gl, glslProgram, scene, material, camera, mesh, lights) {
+    super.setUniforms(gl, glslProgram, scene, material, camera, mesh, lights);
+
+  }
+
+}
+
+GLBoost['PBRPrincipledShader'] = PBRPrincipledShader;
