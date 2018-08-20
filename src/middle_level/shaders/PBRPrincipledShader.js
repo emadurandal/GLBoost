13f6518e86555a89e5b131800f5a3b1a810947b3
Yuki Shimada <emadurandal@gmail.com>
2018-08-20 11:01:50 +0900

    Implementing PBRPrincipledShader

--- src/middle_level/shaders/PBRPrincipledShader.js
+++ src/middle_level/shaders/PBRPrincipledShader.js
@@ -81,13 +81,13 @@ export class PBRPrincipledShaderSource {
     // That is, Unreal Engine based approach, but modified to use alphaRoughness (squared artist's roughness parameter),
     // and based on 'Separable Masking and Shadowing' approximation (propesed by Christophe Schlick)
     // https://www.cs.virginia.edu/~jdl/bib/appearance/analytic%20models/schlick94b.pdf
-    float g_shielding(float NH, float NV, float NL, float VH, float alphaRoughness) {
+    float g_shielding(float NL, float NV, float alphaRoughness) {
       float r = alphaRoughness;
 
-      // Local Shadowing using Smith Masking Function (Schlick's approximation version)
+      // Local Shadowing using "Schlick-Smith" Masking Function
       float localShadowing = 2.0 * NL / (NL + sqrt(r * r + (1.0 - r * r) * (NL * NL)));
       
-      // Local Masking using Smith Masking Function (Schlick's approximation version)
+      // Local Masking using "Schlick-Smith" Masking Function
       float localMasking = 2.0 * NV / (NV + sqrt(r * r + (1.0 - r * r) * (NV * NV)));
       
       return localShadowing * localMasking;
@@ -102,11 +102,11 @@ export class PBRPrincipledShaderSource {
     `;
 
     shaderText += `
-    float cook_torrance_specular_brdf(float n, float NH, float NV, float NL, float VH, float power) {    
-      float D = d_torrance_reiz(NH, power);
-      float G = g_shielding(NH, NV, NL, VH);
-      float F = fresnel(n, VH);
-      return D*G*F/(4*NL*NV);
+    float cook_torrance_specular_brdf(float f0, float NH, float NL, float NV, float alphaRoughness) {    
+      float D = d_ggx(NH, alphaRoughness);
+      float G = g_shielding(NL, NV, alphaRoughness);
+      float F = fresnel(f0, LH);
+      return D*G*F/(4.0*NL*NV);
     }
     `;
 
@@ -127,6 +127,14 @@ export class PBRPrincipledShaderSource {
       shaderText +=      Shader._generateShadowingStr(gl, i, isShadowEnabledAsTexture);
       shaderText += `    float diffuse = max(dot(lightDirection, normal), 0.0);\n`;
       shaderText += `    rt0 += spotEffect * vec4(visibility, visibility, visibility, 1.0) * Kd * lightDiffuse[${i}] * vec4(diffuse, diffuse, diffuse, 1.0) * surfaceColor;\n`;
+      shaderText += `    vec3 viewDirection = normalize(viewPosition_world - v_position_world);\n`;
+      shaderText += '    vec3 halfVector = normalize(lightDirection + viewDirection);\n';
+      shaderText += '    float NH = dot(normal, halfVector);\n';
+      shaderText += '    float NV = dot(normal, viewDirection);\n';
+      shaderText += '    float NL = dot(normal, lightDirection);\n';
+      shaderText += '    float VH = dot(viewDirection, halfVector);\n';
+
+      shaderText += `    float specular = cook_torrance_specular_brdf(f0, NH, NL, NV, alphaRoughness);\n`;
       shaderText += `  }\n`;
     }
     shaderText += '  rt0.xyz += ambient.xyz;\n';
