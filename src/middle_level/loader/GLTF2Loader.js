7b863bc138f25f594319c7805ea5e89f7064603c
Yuki Shimada <emadurandal@gmail.com>
2018-09-26 14:11:11 +0900

    Only Mac to use cors-anywhare.glboost.org

--- src/middle_level/loader/GLTF2Loader.js
+++ src/middle_level/loader/GLTF2Loader.js
@@ -101,20 +101,23 @@ export default class GLTF2Loader {
     };
 
     (function() {
-      var cors_api_host = 'cors-anywhere.glboost.org';
-      var cors_api_url = 'https://' + cors_api_host + '/';
-      var slice = [].slice;
-      var origin = window.location.protocol + '//' + window.location.host;
-      var open = XMLHttpRequest.prototype.open;
-      XMLHttpRequest.prototype.open = function() {
-          var args = slice.call(arguments);
-          var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
-          if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
-              targetOrigin[1] !== cors_api_host) {
-              args[1] = cors_api_url + args[1];
-          }
-          return open.apply(this, args);
-      };
+      const userAgent = window.navigator.userAgent.toLowerCase();
+      if(userAgent.indexOf('safari') !== -1) {
+        var cors_api_host = 'cors-anywhere.glboost.org';
+        var cors_api_url = 'https://' + cors_api_host + '/';
+        var slice = [].slice;
+        var origin = window.location.protocol + '//' + window.location.host;
+        var open = XMLHttpRequest.prototype.open;
+        XMLHttpRequest.prototype.open = function() {
+            var args = slice.call(arguments);
+            var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+            if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
+                targetOrigin[1] !== cors_api_host) {
+                args[1] = cors_api_url + args[1];
+            }
+            return open.apply(this, args);
+        };
+      }
     })();
 
     if (options && options.files) {
