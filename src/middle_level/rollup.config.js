b6380fe9ec1da8751c053f0553dded10e9438af6
Yuki Shimada <emadurandal@gmail.com>
2018-10-08 08:11:06 +0900

    Improve build settings (enable test with flowtype)

--- src/middle_level/rollup.config.js
+++ src/middle_level/rollup.config.js
@@ -1,3 +1,4 @@
+import babel from 'rollup-plugin-babel';
 import flow from 'rollup-plugin-flow';
 
 export default {
@@ -8,6 +9,24 @@ export default {
     format: 'umd'
   },
   plugins: [
-    flow()
+    flow(),
+    babel({
+      exclude: 'node_modules/**',
+      babelrc: false,
+      // use recommended babel-preset-env without es modules enabled
+      // and with possibility to set custom targets e.g. { node: '8' }
+      presets: [['env', {
+        modules: false,
+        plugins: ['babel-plugin-rewire']
+      }]],
+      // solve a problem with spread operator transpilation https://github.com/rollup/rollup/issues/281
+      plugins: [
+        'external-helpers',
+        //'babel-plugin-transform-object-rest-spread'
+      ],
+      // removes comments from output
+      comments: false,
+      externalHelpers: true
+    })
   ]
 }
