ef5ba315d358b9016f649ac160316ff769042130
Yuki Shimada <emadurandal@gmail.com>
2018-10-08 10:47:10 +0900

    Fix build setting to fix fatal runtime error (No use babel again)

--- src/middle_level/rollup.config.js
+++ src/middle_level/rollup.config.js
@@ -1,4 +1,3 @@
-import babel from 'rollup-plugin-babel';
 import flow from 'rollup-plugin-flow';
 
 export default {
@@ -9,24 +8,6 @@ export default {
     format: 'umd'
   },
   plugins: [
-    flow(),
-    babel({
-      exclude: 'node_modules/**',
-      babelrc: false,
-      // use recommended babel-preset-env without es modules enabled
-      // and with possibility to set custom targets e.g. { node: '8' }
-      presets: [['env', {
-        modules: false,
-        plugins: ['babel-plugin-rewire']
-      }]],
-      // solve a problem with spread operator transpilation https://github.com/rollup/rollup/issues/281
-      plugins: [
-        'external-helpers',
-        //'babel-plugin-transform-object-rest-spread'
-      ],
-      // removes comments from output
-      comments: false,
-      externalHelpers: true
-    })
+    flow()
   ]
 }
